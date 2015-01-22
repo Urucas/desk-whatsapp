e03caf7e9f622f70af4f4962833b0d27794443cb
vrunoa <vruno.alassia@gmail.com>
2015-01-21 23:39:58 -0300

    add Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+var parseBuildPlatforms = function(argumentPlatform) {
+	// this will make it build no platform when the platform option is specified
+	// without a value which makes argumentPlatform into a boolean
+	var inputPlatforms = argumentPlatform || process.platform + ";" + process.arch;
+
+	// Do some scrubbing to make it easier to match in the regexes bellow
+	inputPlatforms = inputPlatforms.replace("darwin", "mac");
+	inputPlatforms = inputPlatforms.replace(/;ia|;x|;arm/, "");
+
+	var buildAll = /^all$/.test(inputPlatforms);
+
+	var buildPlatforms = {
+		mac: /mac/.test(inputPlatforms) || buildAll,
+		win: /win/.test(inputPlatforms) || buildAll,
+		linux32: /linux32/.test(inputPlatforms) || buildAll,
+		linux64: /linux64/.test(inputPlatforms) || buildAll
+	};
+
+	return buildPlatforms;
+};
+
+module.exports = function(grunt) {
+	"use strict";
+
+	var buildPlatforms = parseBuildPlatforms(grunt.option('platforms'));
+	var currentVersion = grunt.file.readJSON('package.json').version;
+
+	grunt.initConfig({
+    nodewebkit: {
+      options: {
+        version: '0.10.0',
+        buildDir: './build', // Where the build version of my node-webkit app is saved
+        platforms: ['osx'] // These are the platforms that we want to build
+      },
+      src: './src/**/*' // Your node-webkit app
+    }
+	});
+
+  grunt.loadNpmTasks('grunt-node-webkit-builder');
+  grunt.registerTask('default', ['nodewebkit']);
+
+};
