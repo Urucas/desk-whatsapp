362f8cce8af9cdd485314e9bdc665aa6fc008e03
vrunoa <vruno.alassia@gmail.com>
2015-01-27 23:42:27 -0300

    update app.js add try@catch for debug settings

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,14 @@
-var jf = require("jsonfile"), config;
-		config = jf.readFileSync("package.json");
 
 var gui = require("nw.gui");
 
+try {
+  var jf = require("jsonfile");
+  var	config = jf.readFileSync("./package.json");
 
-if(config.debug == 1)	{
-	gui.Window.get().showDevTools();
-}
-
+  if(config != undefined && config.debug == 1)	{
+	  gui.Window.get().showDevTools();
+  }
+}catch(e){}
 
 var mb = new gui.Menu({type:"menubar"});
 if (process.platform === "darwin") {  // this should indicate you're on Mac OSX
